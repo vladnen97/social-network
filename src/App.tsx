6db6e5f47caac43a4336566b76e6fd9b755bc0bd
vladnen97 <darkdescent1991@gmail.com>
2023-02-05 10:37:23 +0300

    replace browser router

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,15 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navigation} from "./components/Navbar/Navigation";
 import {Content} from "./components/Content/Content";
-import {BrowserRouter} from "react-router-dom";
 
 
 function App(props: any) {
     return (
-        <BrowserRouter>
             <div className={"app-wrapper"}>
                 <Header/>
                 <Navigation/>
                 <Content state={props.state}/>
             </div>
-        </BrowserRouter>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import 'antd/dist/antd.css'
 import App from './App';
-import {state} from "./redux/state";
-
+import {state} from './redux/state';
 
 
 ReactDOM.render(
-    <App state={state} />,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App state={state}/>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
