04d16c6dd66897e7efc60515bbd5c956bc0882a7
vladnen97 <darkdescent1991@gmail.com>
2023-02-09 22:42:40 +0300

    add styles, add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import h from './Header.module.css';
+import {SvgIcon} from '../Navbar/SvgIcon';
 
 export function Header() {
     return (
         <header className={h.header}>
-            <img className={h.logo}
-                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmg6VbQr7k4bE8m1sGjODK19nEZn-UKVChBg&usqp=CAU"
-                 alt="logo"
-            />
+            <div className={h.logo}>
+                <SvgIcon id={'logo'}/>
+            </div>
         </header>
     );
 }
