e417c746243b12602891b2d0aa2c59baf778e90b
vladnen97 <darkdescent1991@gmail.com>
2023-02-10 04:44:08 +0300

    user card and styles for it

---
+++ src/components/Content/Profile/Wall/UserCard/CardUser.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Avatar} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import s from './CardUser.module.css';
+
+export function CardUser() {
+    return (
+        <div className={s.cardUser}>
+            <Avatar size={64} icon={<UserOutlined/>}/>
+            <div className={s.info}>
+                <div className={s.name}>
+                    Vlad Nenashkin
+                </div>
+                <div className={s.text}>
+                    time when sended
+                </div>
+            </div>
+        </div>
+    );
+}
+
