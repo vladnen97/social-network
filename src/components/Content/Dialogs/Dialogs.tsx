18fd27cc7569a03c05856ae9d6268692d1d1c346
vladnen97 <darkdescent1991@gmail.com>
2023-02-11 11:09:28 +0300

    add styles, import new components

--- src/components/Content/Dialogs/Dialogs.tsx
+++ src/components/Content/Dialogs/Dialogs.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import d from './Dialogs.module.css';
-
+import {Dialog} from './Dialog/Dialog';
+import {Message} from './Message/Message';
 
 
 export function Dialogs() {
 
 
     return (
-        <div className={d.content}>
-            <div className={d.chat}>
+        <div className={d.chat}>
 
+            <div className={d.messages}>
+                <Message/>
+                <Message/>
+                <Message/>
+                <Message/>
             </div>
-            <div className={d.dialogs}>
 
+            <div className={d.dialogs}>
+                <Dialog/>
+                <Dialog/>
+                <Dialog/>
+                <Dialog/>
             </div>
         </div>
     );
