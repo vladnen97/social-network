c9c0073fdea272a4db78bc3f2974db9c31918fe7
vladnen97 <darkdescent1991@gmail.com>
2023-02-11 09:56:27 +0300

    rework routings

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -7,7 +7,7 @@ export function Navigation() {
     return (
         <nav className={n.nav}>
             <Item link ={"/profile"} title ={'Profile'} logo={'user'}/>
-            <Item link = {"/news"} title ={'Friends'} logo={'group'}/>
+            <Item link = {"/friends"} title ={'Friends'} logo={'group'}/>
             <Item link = {"/dialogs"} title ={'Messages'} logo={'chat'}/>
             <Item link = {"/music"} title ={'Music'} logo={'music'}/>
             <Item link = {"/settings"} title ={'Settings'} logo={'settings'}/>
