c42a87e5f00a4de3b4385ee38b9f0e34523bbb90
vladnen97 <darkdescent1991@gmail.com>
2023-02-13 19:04:26 +0300

    add news delete friends

---
+++ src/components/Content/News/News.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from "./News.module.css";
+
+export function News() {
+    return (
+        <div>
+            news feed
+        </div>
+    );
+}
--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -7,8 +7,8 @@ export function Navigation() {
     return (
         <nav className={n.nav}>
             <Item link ={"/profile"} title ={'Profile'} logo={'user'}/>
-            <Item link = {"/friends"} title ={'Friends'} logo={'group'}/>
             <Item link = {"/dialogs"} title ={'Messages'} logo={'chat'}/>
+            <Item link = {"/news"} title ={'News'} logo={'group'}/>
             <Item link = {"/music"} title ={'Music'} logo={'music'}/>
             <Item link = {"/settings"} title ={'Settings'} logo={'settings'}/>
         </nav>
