f824f5fa403188957af96f381d8c3175e519bece
vladnen97 <darkdescent1991@gmail.com>
2023-02-13 19:05:31 +0300

    add message sender

--- src/components/Content/Dialogs/Dialogs.tsx
+++ src/components/Content/Dialogs/Dialogs.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import d from './Dialogs.module.css';
+import s from './Dialogs.module.css';
 import {Dialog} from './Dialog/Dialog';
 import {Message} from './Message/Message';
+import {Divider} from 'antd';
 
 
 export function Dialogs() {
 
 
     return (
-        <div className={d.chat}>
+        <div className={s.content}>
 
-            <div className={d.messages}>
-                <Message/>
-                <Message/>
-                <Message/>
-                <Message/>
+            <div className={s.chat}>
+                <div className={s.messages}>
+                    <Message/>
+                    <Message/>
+                    <Message/>
+                    <Message/>
+                </div>
+
+                <Divider className={s.divider}/>
+
+                <div className={s.sendMessage}>
+                    <textarea className={s.textArea} placeholder={'Write a message...'}></textarea>
+                    <button onClick={() => alert('not work yet')} className={s.sendButton}> send </button>
+                </div>
             </div>
 
-            <div className={d.dialogs}>
+            <div className={s.dialogs}>
                 <Dialog/>
                 <Dialog/>
                 <Dialog/>
