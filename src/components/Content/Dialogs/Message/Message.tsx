97f117b11bb05992f9e7741733ad6bd4450c9285
vladnen97 <darkdescent1991@gmail.com>
2023-02-17 21:28:08 +0300

    rework message

--- src/components/Content/Dialogs/Message/Message.tsx
+++ src/components/Content/Dialogs/Message/Message.tsx
@@ -5,11 +5,11 @@ import s from './Message.module.css';
 export function Message() {
     return (
         <div className={s.content}>
-            <div className={s.userInfo}>
-                <Avatar size={64}> John </Avatar>
-                <div className={s.status}>22:00</div>
+            <div className={s.messageInfo}>
+                <Avatar size={48}> John </Avatar>
+                <div className={s.message}>some text for message some text for message </div>
             </div>
-            <div className={s.message}>some text for message some text for message </div>
+            <div className={s.status}>22:00</div>
         </div>
     );
 }
