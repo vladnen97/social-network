35f3276e9bdf6d2cbbf16bfcd3afef82dc3b2fd4
vladnen97 <darkdescent1991@gmail.com>
2023-02-17 20:27:15 +0300

    remove unnecessary data

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,29 +1,5 @@
 
-export let state = {
-    profilePage: {
-        posts: [
-            {id: 1, name: "Vlad Nenashkin", date: "20 Junary 2023", text: 'It is my first post', likes: 6},
-            {id: 2, name: "Vlad Nenashkin", date: "23 Junary 2023", text: 'Hi, how are you today?', likes: 17},
-            {id: 3, name: "Vlad Nenashkin", date: "25 Junary 2023", text: 'Extra post', likes: 0},
-            {id: 4, name: "Vlad Nenashkin", date: "27 Junary 2023", text: 'bla bla bla', likes: 2}
-        ]
-    },
-    dialogsPage: {
-        messages: [
-            {id: 1, text: "Hi"},
-            {id: 2, text: "How are yot today?"},
-            {id: 3, text: "Hello World"},
-            {id: 4, text: "Yo"},
-            {id: 5, text: "Some text here"}
-        ],
-        dialogs: [
-            {id: 1, name: "Misha Kuznetsov"},
-            {id: 2, name: "Vasya Pupkin"},
-            {id: 3, name: "Dima Sokolov"},
-            {id: 4, name: "Lena Golovach"},
-            {id: 5, name: "Nakatika Naebuka"}
-        ]
-    },
-    friendsPage: {}
+export const state = {
+
 }
 
