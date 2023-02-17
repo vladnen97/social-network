22b489a6a4b992a43c96d4299c62614ac64284b8
vladnen97 <darkdescent1991@gmail.com>
2023-02-17 22:43:34 +0300

    main status component

---
+++ src/components/Content/Profile/Header/HeaderComp/MainStatus.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from '../Header.module.css';
+
+export function MainStatus() {
+    return (
+        <div className={s.status}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+        </div>
+    );
+}
