3b4effb928526bdc752eebd6cc3cda7b7a44da4a
vladnen97 <darkdescent1991@gmail.com>
2023-02-17 20:10:56 +0300

    moved the navigation to the component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import s from './SideBar.module.css';
-import {Item} from './Item/Item';
 import {Avatar, Divider} from 'antd';
 import {UserOutlined} from '@ant-design/icons';
+import {Nav} from './Nav/Nav';
 
 
 export function SideBar() {
     return (
-        <nav className={s.sideBar}>
-            <div className={s.nav}>
-                <Item link={'/profile'} title={'Profile'} logo={'user'}/>
-                <Item link={'/dialogs'} title={'Messages'} logo={'chat'}/>
-                <Item link={'/news'} title={'News'} logo={'news'}/>
-                <Item link={'/music'} title={'Music'} logo={'music'}/>
-                <Item link={'/settings'} title={'Settings'} logo={'settings'}/>
-            </div>
+        <div className={s.sideBar}>
+            <Nav/>
             <div className={s.following}>  {/*вынести в отдельную компоненту*/}
                 <div className={s.followingHeader}>
                     <h2>I'm Following</h2>
@@ -26,9 +20,10 @@ export function SideBar() {
                     <div className={s.user}>
                         <Avatar size={48} icon={<UserOutlined/>}/>
                         <div className={s.userName}>Name K.</div>
-                    </div>  {/*вынести в отдельную компоненту*/}
+                    </div>
+                    {/*вынести в отдельную компоненту*/}
                 </div>
             </div>
-        </nav>
+        </div>
     );
 }
