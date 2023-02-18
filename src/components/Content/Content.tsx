1c31cd0f67c05bfacdf3a5a6567614b4fdb9adbb
vladnen97 <darkdescent1991@gmail.com>
2023-02-18 16:04:59 +0300

    add props typization

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,13 +6,15 @@ import {Dialogs} from "./Dialogs/Dialogs";
 import {News} from "./News/News";
 import {Music} from "./Music/Music";
 
+type PropsType = {
 
+}
 
-export function Content(props: any) {
+export function Content({content}: any) {
     return (
             <div className={c.content}>
-                <Route path = '/profile' render={() => <Profile/> }/>
-                <Route path = '/dialogs' render={() => <Dialogs /> }/>
+                <Route path = '/profile' render={() => <Profile localState={content.profilePage}/> }/>
+                <Route path = '/dialogs' render={() => <Dialogs localState={content.dialogsPage}/> }/>
                 <Route path = '/news' render={() => <News/> }/>
                 <Route path = '/music' render={() => <Music/> }/>
             </div>
