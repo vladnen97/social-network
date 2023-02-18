2ff791e47d06c949f7ec66530e8b8d9a5e714c4e
vladnen97 <darkdescent1991@gmail.com>
2023-02-18 16:04:12 +0300

    remove all data from component, add props typization

--- src/components/Content/Dialogs/Dialogs.tsx
+++ src/components/Content/Dialogs/Dialogs.tsx
@@ -4,34 +4,35 @@ import {Dialog} from './Dialog/Dialog';
 import {Message} from './Message/Message';
 import {Divider} from 'antd';
 
+type PropsType = {
+    localState: {
+        messages: Array<any>
+        dialogs: Array<any>
+    }
+}
 
-export function Dialogs() {
+export function Dialogs({localState: {messages, dialogs}}: PropsType) {
 
+    const mappedDialogs = dialogs.map(el => <Dialog key={el.id} name={el.name} url={el.url} status={el.status}/>);
+    const mappedMessages = messages.map(el => <Message key={el.id} url={el.url} text={el.text} status={el.status}/>);
 
     return (
         <div className={s.content}>
-
             <div className={s.chat}>
                 <div className={s.messages}>
-                    <Message/> {/*разбить на 2 компоненты: UserMessages, FriendMessages*/}
-                    <Message/>
-                    <Message/>
-                    <Message/>
+                    {mappedMessages}
                 </div>
 
                 <Divider className={s.divider}/>
 
                 <div className={s.sendMessage}> {/*доработать*/}
                     <textarea className={s.textArea} placeholder={'Write a message...'}></textarea>
-                    <button onClick={() => alert('not work yet')} className={s.sendButton}> send </button>
+                    <button onClick={() => alert('not work yet')} className={s.sendButton}> send</button>
                 </div>
             </div>
 
             <div className={s.dialogs}>
-                <Dialog/> {/*замапить*/}
-                <Dialog/>
-                <Dialog/>
-                <Dialog/>
+                {mappedDialogs}
             </div>
         </div>
     );
