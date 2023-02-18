3d3da826d374a17183e86c420a6650046d5582f1
vladnen97 <darkdescent1991@gmail.com>
2023-02-18 16:04:33 +0300

    remove all data from component, add props typization

--- src/components/Content/Profile/Profile.tsx
+++ src/components/Content/Profile/Profile.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import {Header} from "./Header/Header";
 import {Wall} from "./Wall/Wall";
 
+type PropsType = {
+    localState: {
+        header: {
+            online: boolean
+            url: string
+            name: string,
+            status: string,
+            iconId: string,
+            title: string,
+        }
+        posts: Array<any>
+    }
+}
 
-export function Profile(props: any) {
+export function Profile(props: PropsType) {
     return (
         <div className={'profile'}>
-            <Header/>
-            <Wall/>
+            <Header header={props.localState.header}/>
+            <Wall posts={props.localState.posts}/>
         </div>
     );
 }
