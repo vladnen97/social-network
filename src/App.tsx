bd2288e199aef3f71534b94461c3b386045884d4
vladnen97 <darkdescent1991@gmail.com>
2023-02-20 17:13:16 +0300

    add typization

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {SideBar} from "./components/SideBar/SideBar";
 import {Content} from "./components/Content/Content";
+import {StateType} from './redux/state';
 
+type PropsType = {
+    state: StateType
+}
 
-function App({state}: any) {
+function App({state}: PropsType) {
     return (
             <div className={"app-wrapper"}>
                 <Header/>
                 <SideBar sideBar={state.sideBar}/>
-                <Content content={state.content}/>
+                <Content profile={state.profilePage} dialogs={state.dialogsPage}/>
             </div>
     );
 }
