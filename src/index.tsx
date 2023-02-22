122b80ed5a53d3ba66693487f887bfd7c884badc
vladnen97 <darkdescent1991@gmail.com>
2023-02-22 17:33:20 +0300

    import rerender func and call it

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
 import App from './App';
-import {state} from './redux/state';
+import {state, addPost} from './redux/state';
+import {rerenderEntireTree} from './render';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App state={state}/>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+rerenderEntireTree();
