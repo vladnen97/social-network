7ad26942f83e0ef3cc63d03691284816261132bb
vladnen97 <darkdescent1991@gmail.com>
2023-03-06 03:07:47 +0300

    change props and refactor handler func

--- src/components/Content/Profile/Wall/Wall.tsx
+++ src/components/Content/Profile/Wall/Wall.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import s from "./Wall.module.css"
 import {Post} from "./Post/Post";
 import {SubmitPost} from './SubmitPost/SubmitPost';
-import {PostType} from '../../../../redux/state';
+import {ActionsType, PostType} from '../../../../redux/state';
 
 type PropsType = {
     postTextValue: string
     posts: Array<PostType>
-    addPost: () => void
-    changePostText: (value: string) => void
+    dispatch: (action: ActionsType) => void
 }
 
-export function Wall({posts, addPost, postTextValue, changePostText}: PropsType) {
+export function Wall({posts, postTextValue, dispatch}: PropsType) {
 
     const wallElements = posts.map( (p) => {
         return (
@@ -21,7 +20,7 @@ export function Wall({posts, addPost, postTextValue, changePostText}: PropsType)
 
     return (
         <div className={s.wall}>
-            <SubmitPost addPost={addPost} postTextValue={postTextValue} changePostText={changePostText}/>
+            <SubmitPost postTextValue={postTextValue} dispatch={dispatch}/>
             { wallElements }
         </div>
     );
