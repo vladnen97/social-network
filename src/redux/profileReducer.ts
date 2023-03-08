1ac6363b58b4de1847dda5f41a125db33c31c993
vladnen97 <darkdescent1991@gmail.com>
2023-03-08 11:40:29 +0300

    profile page reducer

---
+++ src/redux/profileReducer.ts
@@ -0,0 +1,31 @@
+import {ActionsType, PostType, ProfilePageType} from './state';
+import {v1} from 'uuid';
+
+
+function profileReducer(state: ProfilePageType, action: ActionsType) {
+
+    switch (action.type) {
+        case 'ADD-POST':
+            const newPost: PostType = {
+                id: v1(),
+                name: state.header.name,
+                date: new Date().toLocaleString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, 12),
+                postContent: state.postTextValue,
+                likes: 0,
+                comments: 0
+            }
+            state.posts.unshift(newPost);
+            state.postTextValue = '';
+            break;
+        case 'UPDATE-NEW-POST-TEXT':
+            state.postTextValue = action.newText;
+            break;
+    }
+
+    return state
+}
+
+export const addPostActionCreator = () => ({type: 'ADD-POST'} as const);
+export const ChangeNewPostTextActionCreator = (newText: string) => ({type: 'UPDATE-NEW-POST-TEXT', newText: newText} as const);
+
+export default profileReducer;
