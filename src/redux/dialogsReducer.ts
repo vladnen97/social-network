eee8add0d963c31932404aef3a7597b04668ec8f
vladnen97 <darkdescent1991@gmail.com>
2023-03-13 15:15:10 +0300

    add initial state

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -2,7 +2,74 @@ import {ActionsType, DialogsPageType, MessageType} from './store';
 import {v1} from 'uuid';
 
 
-function dialogsReducer(state: DialogsPageType, action: ActionsType) {
+const initialState: DialogsPageType = {
+    newMessageText: '',
+    messages: [
+        {
+            id: v1(),
+            text: 'Hi',
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zsO-WB5sqXt2_4XGhgNqeecBwQ2dm2dTFcV4NBR0hBIK9nlKRuIz8HUwEo-eEteJBm4&usqp=CAU',
+            status: '19:37'
+        },
+        {
+            id: v1(),
+            text: 'How are you today?',
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zsO-WB5sqXt2_4XGhgNqeecBwQ2dm2dTFcV4NBR0hBIK9nlKRuIz8HUwEo-eEteJBm4&usqp=CAU',
+            status: '19:38'
+        },
+        {
+            id: v1(),
+            text: 'Hi, I\'m fine, wby?',
+            url: 'https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg',
+            status: '20:02'
+        },
+        {
+            id: v1(),
+            text: 'Im fine too, thanks',
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zsO-WB5sqXt2_4XGhgNqeecBwQ2dm2dTFcV4NBR0hBIK9nlKRuIz8HUwEo-eEteJBm4&usqp=CAU',
+            status: '21:13'
+        },
+        {
+            id: v1(),
+            text: 'Cool Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            url: 'https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg',
+            status: '22:15'
+        }
+    ],
+    dialogs: [
+        {
+            id: v1(),
+            name: 'Misha Kuznetsov',
+            url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-zsO-WB5sqXt2_4XGhgNqeecBwQ2dm2dTFcV4NBR0hBIK9nlKRuIz8HUwEo-eEteJBm4&usqp=CAU',
+            status: 'here will be last message',
+            path: '1'
+        },
+        {
+            id: v1(),
+            name: 'Dasha Bukina',
+            url: 'https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg',
+            status: 'here will be last message',
+            path: '2'
+        },
+        {
+            id: v1(),
+            name: 'Dima Sokolov',
+            url: 'https://www.shutterstock.com/image-photo/headshot-portrait-smiling-african-american-260nw-1443249407.jpg',
+            status: 'here will be last message',
+            path: '3'
+        },
+        {
+            id: v1(),
+            name: 'ELena Golovach',
+            url: 'https://media.istockphoto.com/id/1381221247/photo/beautiful-afro-girl-with-curly-hairstyle.jpg?b=1&s=170667a&w=0&k=20&c=0x91osZOkL8EfhTEEGNa2EeCGN2gdMTNULOsUFW_0i4=',
+            status: 'here will be last message',
+            path: '4'
+        },
+    ],
+}
+
+
+function dialogsReducer(state: DialogsPageType = initialState, action: ActionsType): DialogsPageType {
 
     switch (action.type) {
         case 'ADD-MESSAGE':
--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -1,15 +1,63 @@
-import {ActionsType, PostType, ProfilePageType} from './store';
 import {v1} from 'uuid';
+import {ActionsType, PostType, ProfilePageType} from './store';
 
 
-function profileReducer(state: ProfilePageType, action: ActionsType) {
+const initialState: ProfilePageType= {
+    postTextValue: '',
+    header: {
+        online: true,
+        url: 'https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg',
+        name: 'Name Surname',
+        status: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        iconId: 'location',
+        title: 'Saint-Petersburg',
+    },
+    posts: [
+        {
+            id: v1(),
+            name: 'Name Surname',
+            date: '27 Junary 2023',
+            postContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            likes: 13,
+            comments: 0
+        },
+        {
+            id: v1(),
+            name: 'Name Surname',
+            date: '25 Junary 2023',
+            postContent: 'nibh sit amet commodo nulla facilisi nullam vehicula ipsum a',
+            likes: 1,
+            comments: 0
+        },
+        {
+            id: v1(),
+            name: 'Name Surname',
+            date: '22 Junary 2023',
+            postContent: 'Hello, how are you today',
+            likes: 9,
+            comments: 0
+        },
+        {
+            id: v1(),
+            name: 'Name Surname',
+            date: '20 Junary 2023',
+            postContent: ' It is my first post',
+            likes: 23,
+            comments: 0
+        },
+
+    ]
+}
+
+
+function profileReducer(state: ProfilePageType = initialState, action: ActionsType): ProfilePageType {
 
     switch (action.type) {
         case 'ADD-POST':
             const newPost: PostType = {
                 id: v1(),
                 name: state.header.name,
-                date: new Date().toLocaleString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, 12),
+                date: new Date().toLocaleString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, 13),
                 postContent: state.postTextValue,
                 likes: 0,
                 comments: 0
