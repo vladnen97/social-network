544e28db7f260deb941d228db423e0e95cda00f9
vladnen97 <darkdescent1991@gmail.com>
2023-03-14 22:40:35 +0300

    wrapp Dialogs comp in Consumer

--- src/components/Content/Dialogs/DialogsContainer.tsx
+++ src/components/Content/Dialogs/DialogsContainer.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import {StoreType} from '../../../redux/store';
 import {addMessageActionCreator, changeNewMessageTextActionCreator} from '../../../redux/dialogsReducer';
 import {Dialogs} from './Dialogs';
-
-type PropsType = {
-    store: StoreType
-}
+import StoreContext from '../../../storeContext';
 
 
-export function DialogsContainer({store}: PropsType) {
-    const state = store.getState()
-
-    const addNewMessage = (): void => store.dispatch(addMessageActionCreator())
-    const updateNewMessageText = (value: string): void => {
-        store.dispatch(changeNewMessageTextActionCreator(value));
-    }
-
+export function DialogsContainer() {
 
     return (
-        <Dialogs updateNewMessageText={updateNewMessageText}
-                 addNewMessage={addNewMessage}
-                 dialogsData={state.dialogsPage}
-        />
+    <StoreContext.Consumer>
+        {
+            (store) => {
+
+                const dialogsPage = store.getState().dialogsPage
+
+                const addNewMessage = (): void => {
+                    store.dispatch(addMessageActionCreator())
+                }
+                const updateNewMessageText = (value: string): void => {
+                    store.dispatch(changeNewMessageTextActionCreator(value));
+                }
+
+                return <Dialogs updateNewMessageText={updateNewMessageText}
+                                        addNewMessage={addNewMessage}
+                                        dialogsData={dialogsPage}
+                />
+            }
+        }
+    </StoreContext.Consumer>
     )
 }
 
