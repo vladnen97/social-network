936c050026f95a4964fa761a077e151f7c5dd762
vladnen97 <darkdescent1991@gmail.com>
2023-03-14 22:40:40 +0300

    wrapp Dialogs comp in Consumer

--- src/components/Content/Profile/Wall/WallContainer.tsx
+++ src/components/Content/Profile/Wall/WallContainer.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import {addPostActionCreator, ChangeNewPostTextActionCreator} from '../../../../redux/profileReducer';
-import {StoreType} from '../../../../redux/store';
+import StoreContext from '../../../../storeContext';
 import {Wall} from './Wall';
 
 
-type PropsType = {
-    store: StoreType
-}
-
-export function WallContainer({store}: PropsType) {
-
-    const profilePage = store.getState().profilePage
-
-    const addPost = ():void => {
-        store.dispatch(addPostActionCreator());
-    }
-    const onChangeText = (value: string): void => {
-        store.dispatch(ChangeNewPostTextActionCreator(value));
-    }
+export function WallContainer() {
 
     return (
-        <Wall posts={profilePage.posts} addPost={addPost} onChangeText={onChangeText} postTextValue={profilePage.postTextValue}/>
+        <StoreContext.Consumer>
+            {
+                (store) => {
+                    const profilePage = store.getState().profilePage;
+
+                    const addPost = (): void => {
+                        store.dispatch(addPostActionCreator());
+                    }
+                    const onChangeText = (value: string): void => {
+                        store.dispatch(ChangeNewPostTextActionCreator(value));
+                    }
+
+                    return <Wall posts={profilePage.posts}
+                                 addPost={addPost}
+                                 onChangeText={onChangeText}
+                                 postTextValue={profilePage.postTextValue}
+                    />
+                }
+            }
+        </StoreContext.Consumer>
     )
 }
 
