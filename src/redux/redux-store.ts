478ca6c213c9b9dc6284baf4c0029a626517a5f1
vladnen97 <darkdescent1991@gmail.com>
2023-03-19 16:08:57 +0300

    add new reducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,11 +2,13 @@ import {combineReducers, createStore} from 'redux';
 import profileReducer from './profileReducer';
 import dialogsReducer from './dialogsReducer';
 import sideBarReducer from './sideBarReducer';
+import usersReducer from './usersReducer';
 
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
+    usersPage: usersReducer,
     sideBar: sideBarReducer
 })
 
