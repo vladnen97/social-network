2e9f379be84065295631576fa7aa46f6cdfe9c2a
vladnen97 <darkdescent1991@gmail.com>
2023-03-27 19:38:03 +0300

    making a get request using axios, change some styles

--- src/components/Content/Users/Users.tsx
+++ src/components/Content/Users/Users.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import {UsersPropsType} from './UsersContainer';
-import {v1} from 'uuid';
+import axios from 'axios';
 
 export function Users(props: UsersPropsType) {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: v1(),
-                followed: false,
-                fullName: 'Vladislav',
-                status: 'Im a boss',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: v1(),
-                followed: true,
-                fullName: 'Dmitry',
-                status: 'I like football',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: v1(),
-                followed: false,
-                fullName: 'Oksana',
-                status: 'Im pretty',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => {
+                props.setUsers(res.data.items)
+            })
     }
 
     const mappedUsers = props.users.map(el => {
@@ -39,18 +20,23 @@ export function Users(props: UsersPropsType) {
             props.onUnFollowClick(el.id)
         }
 
-        return <div key={el.id} style={ {backgroundColor: '#fff', borderRadius: '20px', padding: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between'} }>
-            <div>
-                <h2>{el.fullName}</h2>
-                <div style={{fontSize: '17px', margin: '10px 5px'}}><em>{el.status}</em></div>
-            </div>
-            <div>
-                <div>{el.location.country}</div>
-                <div>{el.location.city}</div>
+        return <div key={el.id} style={{
+            backgroundColor: '#fff',
+            borderRadius: '20px',
+            padding: '16px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        }}>
+            <img style={{width: '80px', height: '80px', borderRadius: '50%'}}
+                 src={el.photos.small || 'https://placehold.jp/150x150.png'} alt=""/>
+            <div style={{textAlign: 'center'}}>
+                <h2>{el.name}</h2>
+                <div style={{fontSize: '17px', margin: '10px 5px'}}><em>{el.status || 'status...'}</em></div>
             </div>
             {el.followed
                 ? <button onClick={onUnFollowClickHandler}>Unfollow</button>
-                :  <button onClick={onFollowClickHandler}>Follow</button>
+                : <button onClick={onFollowClickHandler}>Follow</button>
             }
         </div>
     })
