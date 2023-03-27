eed281a156cb2d5192842cf9f47978fc8f9030d4
vladnen97 <darkdescent1991@gmail.com>
2023-03-27 20:56:43 +0300

    rewrite func component to class

--- src/components/Content/Users/Users.tsx
+++ src/components/Content/Users/Users.tsx
@@ -2,50 +2,50 @@ import React from 'react';
 import {UsersPropsType} from './UsersContainer';
 import axios from 'axios';
 
-export function Users(props: UsersPropsType) {
+export class Users extends React.Component<UsersPropsType> {
 
-    if (props.users.length === 0) {
+    constructor(props: UsersPropsType) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(res => {
-                props.setUsers(res.data.items)
-            })
+            .then(res => { this.props.setUsers(res.data.items) })
     }
 
-    const mappedUsers = props.users.map(el => {
+    render() {
+        return (
+            <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
+                {
+                    this.props.users.map(el => {
 
-        const onFollowClickHandler = () => {
-            props.onFollowClick(el.id)
-        }
-        const onUnFollowClickHandler = () => {
-            props.onUnFollowClick(el.id)
-        }
+                    const onFollowClickHandler = () => {
+                        this.props.onFollowClick(el.id)
+                    }
+                    const onUnFollowClickHandler = () => {
+                        this.props.onUnFollowClick(el.id)
+                    }
 
-        return <div key={el.id} style={{
-            backgroundColor: '#fff',
-            borderRadius: '20px',
-            padding: '16px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between'
-        }}>
-            <img style={{width: '80px', height: '80px', borderRadius: '50%'}}
-                 src={el.photos.small || 'https://placehold.jp/150x150.png'} alt=""/>
-            <div style={{textAlign: 'center'}}>
-                <h2>{el.name}</h2>
-                <div style={{fontSize: '17px', margin: '10px 5px'}}><em>{el.status || 'status...'}</em></div>
+                    return <div key={el.id} style={{
+                        backgroundColor: '#fff',
+                        borderRadius: '20px',
+                        padding: '16px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                    }}>
+                        <img style={{width: '80px', height: '80px', borderRadius: '50%'}}
+                             src={el.photos.small || 'https://placehold.jp/150x150.png'} alt=""/>
+                        <div style={{textAlign: 'center'}}>
+                            <h2>{el.name}</h2>
+                            <div style={{fontSize: '17px', margin: '10px 5px'}}><em>{el.status || 'status...'}</em>
+                            </div>
+                        </div>
+                        {el.followed
+                            ? <button onClick={onUnFollowClickHandler}>Unfollow</button>
+                            : <button onClick={onFollowClickHandler}>Follow</button>
+                        }
+                    </div>
+                })}
             </div>
-            {el.followed
-                ? <button onClick={onUnFollowClickHandler}>Unfollow</button>
-                : <button onClick={onFollowClickHandler}>Follow</button>
-            }
-        </div>
-    })
-
-
-    return (
-        <div style={{display: 'flex', flexDirection: 'column', gap: '20px'}}>
-            {mappedUsers}
-        </div>
-    );
+        )
+    }
 }
 
