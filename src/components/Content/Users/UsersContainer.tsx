d46e8b19107d364e47e86a36df5f025953961e3d
vladnen97 <darkdescent1991@gmail.com>
2023-04-09 08:56:25 +0300

    pagination callbacks

--- src/components/Content/Users/UsersContainer.tsx
+++ src/components/Content/Users/UsersContainer.tsx
@@ -2,22 +2,37 @@ import {Users} from './Users';
 import {connect} from 'react-redux';
 import {RootStateType} from '../../../redux/redux-store';
 import {Dispatch} from 'redux';
-import {followAC, setUsersAC, unfollowAC, UserType} from '../../../redux/usersReducer';
+import {
+    followAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unfollowAC,
+    UserType
+} from '../../../redux/usersReducer';
 
 type MapStateToPropsType = {
     users: Array<UserType>
+    limit: number
+    totalCount: number
+    currentPage: number
 }
 type MapDispatchToPropsType = {
     onFollowClick: (userId: number) => void
     onUnFollowClick: (userId: number) => void
     setUsers: (users: Array<UserType>) => void
+    onPageClick: (page: number) => void
+    setTotalUsersCount: (count: number) => void
 }
 export type UsersPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 
 const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        limit: state.usersPage.limit,
+        totalCount: state.usersPage.totalCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
 
@@ -25,7 +40,9 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchToPropsType => {
     return {
         onFollowClick: (userId) => {dispatch(followAC(userId))},
         onUnFollowClick: (userId) => {dispatch(unfollowAC(userId))},
-        setUsers: (users) => {dispatch(setUsersAC(users))}
+        setUsers: (users) => {dispatch(setUsersAC(users))},
+        onPageClick: (page) => {dispatch(setCurrentPageAC(page))},
+        setTotalUsersCount: (count) => {dispatch(setTotalUsersCountAC(count))}
     }
 }
 
