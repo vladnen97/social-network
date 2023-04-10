02996168b257dad049f5858f13b8c913f987efee
vladnen97 <darkdescent1991@gmail.com>
2023-04-10 04:28:37 +0300

    new case for user profile, change data type of header

--- src/redux/profileReducer.ts
+++ src/redux/profileReducer.ts
@@ -10,33 +10,37 @@ export type PostType = {
     comments: number
 }
 export type ProfilePageHeaderType = {
-    online: boolean
-    url: string
-    name: string
-    status: string
-    iconId: string
-    title: string
-}
-type ProfilePageType = {
+    aboutMe: string
+    contacts: {
+        facebook: string
+        github: string
+        instagram: string
+        mainLink: string
+        twitter: string
+        vk: string
+        website: string
+        youtube: string
+    }
+    fullName: string
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    photos: { small: string | null, large: string | null }
+    userId: number
+} | null
+export type ProfilePageType = {
     postTextValue: string
     header: ProfilePageHeaderType
     posts: Array<PostType>
 }
-export type ProfilePageActionsType = ReturnType<typeof addPostActionCreator> | ReturnType<typeof ChangeNewPostTextActionCreator>
+export type ProfilePageActionsType = ReturnType<typeof addPost> | ReturnType<typeof ChangeNewPostText> | ReturnType<typeof setUserProfile>
 
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
+const SET_USER_PROFILE = 'SET-USER-PROFILE'
 
 const initialState: ProfilePageType = {
     postTextValue: '',
-    header: {
-        online: true,
-        url: 'https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg',
-        name: 'Name Surname',
-        status: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        iconId: 'location',
-        title: 'Saint-Petersburg',
-    },
+    header: null,
     posts: [
         {
             id: v1(),
@@ -77,11 +81,16 @@ const initialState: ProfilePageType = {
 function profileReducer(state: ProfilePageType = initialState, action: ProfilePageActionsType): ProfilePageType {
 
     switch (action.type) {
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                header: action.profile
+            }
         case ADD_POST:
             const newPost: PostType = {
                 id: v1(),
-                name: state.header.name,
-                date: new Date().toLocaleString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, 13),
+                name: 'Name Surname',
+                date: new Date().toLocaleString('ru-RU', {year: 'numeric', month: 'long', day: 'numeric'}).slice(0, 14),
                 postContent: state.postTextValue,
                 likes: 0,
                 comments: 0
@@ -102,7 +111,8 @@ function profileReducer(state: ProfilePageType = initialState, action: ProfilePa
 
 }
 
-export const addPostActionCreator = () => ({type: ADD_POST} as const);
-export const ChangeNewPostTextActionCreator = (newText: string) => ({type: UPDATE_NEW_POST_TEXT, newText: newText} as const);
+export const addPost = () => ({type: ADD_POST} as const);
+export const ChangeNewPostText = (newText: string) => ({type: UPDATE_NEW_POST_TEXT, newText: newText} as const);
+export const setUserProfile = (profile: ProfilePageHeaderType) => ({type: SET_USER_PROFILE, profile} as const)
 
 export default profileReducer;
