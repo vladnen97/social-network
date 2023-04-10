5feb16e19b8a9df41efce9f619034e3d97c23bb6
vladnen97 <darkdescent1991@gmail.com>
2023-04-10 07:46:46 +0300

    rewrite class comp for func because withRouter too old, using useParams instead

--- src/components/Content/Profile/ProfileContainer.tsx
+++ src/components/Content/Profile/ProfileContainer.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Profile} from './Profile';
-import {ProfilePageHeaderType, ProfilePageType, setUserProfile} from '../../../redux/profileReducer';
+import {ProfilePageHeaderType, setUserProfile} from '../../../redux/profileReducer';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import {RootStateType} from '../../../redux/redux-store';
+import {useParams} from 'react-router-dom';
 
 
 type ProfileInnerContainerPropsType = {
     setUserProfile: (profile: ProfilePageHeaderType) => void
 }
 
-class ProfileInnerContainer extends React.Component<ProfileInnerContainerPropsType, ProfilePageType>{
+function ProfileInnerContainer(props: ProfileInnerContainerPropsType) {
+    const location = useParams<{ userId: string }>()
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/28671`)
+    useEffect(() => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${location.userId ? location.userId : 28671}`)
             .then(res => {
-                this.props.setUserProfile(res.data)
+                props.setUserProfile(res.data)
             })
-    }
+    }, [location])
 
-    render() {
-        return <Profile/>
-    }
+
+    return <Profile/>
 }
 
 
-const mapStateToProps = (state: RootStateType)=> {
+const mapStateToProps = (state: RootStateType) => {
     return {}
 }
 
