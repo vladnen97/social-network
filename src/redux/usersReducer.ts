d84cb9dc70e1c9ef9d6d329e4f5ea0a4f761dc81
vladnen97 <darkdescent1991@gmail.com>
2023-04-14 21:00:44 +0300

    disable button then fetching handler

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -13,16 +13,18 @@ const SET_USERS = 'SET-USERS'
 const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
 const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT'
 const SET_FETCHING = 'SET-FETCHING'
+const SET_FOLLOWING_PROGRESS = 'SET-FOLLOWING-PROGRESS'
 
 const initialState = {
     users: [] as Array<UserType>,
     totalCount: 0,
     currentPage: 1,
-    isFetching: false
+    isFetching: false,
+    followingInProgress: [] as number[]
 }
 
 
-export type UsersPageActionType = ReturnType<typeof follow> | ReturnType<typeof unfollow> | ReturnType<typeof setUsers> | ReturnType<typeof setCurrentPage> | ReturnType<typeof setTotalUsersCount> | ReturnType<typeof setIsFetching>
+export type UsersPageActionType = ReturnType<typeof follow> | ReturnType<typeof unfollow> | ReturnType<typeof setUsers> | ReturnType<typeof setCurrentPage> | ReturnType<typeof setTotalUsersCount> | ReturnType<typeof setIsFetching> | ReturnType<typeof toggleFollowingProgress>
 export type UsersPageType = typeof initialState
 
 
@@ -58,6 +60,13 @@ function usersReducer(state: UsersPageType = initialState, action: UsersPageActi
                 ...state,
                 isFetching: action.value
             }
+        case SET_FOLLOWING_PROGRESS:
+            return {
+               ...state,
+               followingInProgress: action.isFetching
+                   ? [action.userId, ...state.followingInProgress]
+                   : state.followingInProgress.filter(el => el !== action.userId)
+            }
         default:
             return state
     }
@@ -70,5 +79,6 @@ export const setUsers = (users: Array<UserType>) => ({type : SET_USERS, users} a
 export const setCurrentPage = (page: number) => ({type : SET_CURRENT_PAGE, page} as const)
 export const setTotalUsersCount = (count: number) => ({type: SET_TOTAL_USERS_COUNT, count} as const)
 export const setIsFetching = (value: boolean) => ({type: SET_FETCHING, value} as const)
+export const toggleFollowingProgress = (isFetching: boolean, userId: number) => ({type: SET_FOLLOWING_PROGRESS, isFetching, userId} as const)
 
 export default usersReducer;
