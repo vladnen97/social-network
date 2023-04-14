576944ece0dc00922c48865e6b13b2a0e170fd22
vladnen97 <darkdescent1991@gmail.com>
2023-04-14 13:15:58 +0300

    add auth to root state

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,13 +3,15 @@ import profileReducer from './profileReducer';
 import dialogsReducer from './dialogsReducer';
 import sideBarReducer from './sideBarReducer';
 import usersReducer from './usersReducer';
+import {authReducer} from './authReducer';
 
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    sideBar: sideBarReducer
+    sideBar: sideBarReducer,
+    auth: authReducer
 })
 
 export type RootStateType = ReturnType<typeof rootReducer>
@@ -18,3 +20,6 @@ const store: Store<RootStateType> = createStore(rootReducer);
 
 
 export default store;
+
+//@ts-ignore
+window.store= store
