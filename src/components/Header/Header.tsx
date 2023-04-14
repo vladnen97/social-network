9b87de1e13e04dcdc97718589eaca455457658c3
vladnen97 <darkdescent1991@gmail.com>
2023-04-14 13:14:52 +0300

    add props and conditioal render

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import h from './Header.module.css';
+import s from './Header.module.css';
 import {SvgIcon} from '../SideBar/SvgIcon';
+import {NavLink} from 'react-router-dom';
 
-export function Header() {
+export function Header({login}: {login: string | null}) {
     return (
-        <header className={h.header}>
-            <div className={h.logo}>
+        <header className={s.header}>
+            <div className={s.logo}>
                 <SvgIcon id={'logo'}/>
             </div>
+            <h1 className={s.title}>SocialNetwork</h1>
+            {
+                login
+                    ? <h2 className={s.name}>{login}</h2>
+                    : <NavLink to={'/login'} className={s.login}>Login</NavLink>
+            }
         </header>
     );
 }
