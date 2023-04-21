dc73618dd26c0985f30af981ac60db398a4b839e
vladnen97 <darkdescent1991@gmail.com>
2023-04-21 18:34:48 +0300

    apply middleware

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,10 @@
-import {combineReducers, createStore, Store} from 'redux';
+import {applyMiddleware, combineReducers, createStore, Store} from 'redux';
 import profileReducer from './profileReducer';
 import dialogsReducer from './dialogsReducer';
 import sideBarReducer from './sideBarReducer';
 import usersReducer from './usersReducer';
 import {authReducer} from './authReducer';
+import thunk from 'redux-thunk';
 
 
 const rootReducer = combineReducers({
@@ -16,7 +17,7 @@ const rootReducer = combineReducers({
 
 export type RootStateType = ReturnType<typeof rootReducer>
 
-const store: Store<RootStateType> = createStore(rootReducer);
+const store: Store<RootStateType> = createStore(rootReducer, applyMiddleware(thunk));
 
 
 export default store;
