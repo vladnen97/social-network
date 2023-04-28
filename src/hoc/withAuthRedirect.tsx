dd6b8caa35020365584fe7b244643814ff2081fe
vladnen97 <darkdescent1991@gmail.com>
2023-04-28 23:41:00 +0300

    types for custom HOC

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ComponentType} from 'react';
 import {Redirect} from 'react-router-dom';
 import {RootStateType} from '../redux/redux-store';
 import {connect} from 'react-redux';
@@ -6,25 +6,19 @@ import {connect} from 'react-redux';
 type MapStateToProps = {
     isAuth: boolean
 }
-const mapStateToProps = (state: RootStateType): MapStateToProps => {
-    return {
-        isAuth: state.auth.isAuth
-    }
-}
+const mapStateToProps = (state: RootStateType): MapStateToProps => ({isAuth: state.auth.isAuth})
 
 
-export const withAuthRedirect = (Component: any) => {
-    
-    class RedirectComponent extends React.Component<any, {}> {
-        render() {
-            if (!this.props.isAuth) {
-                return <Redirect to={'/login'}/>
-            }
+export function withAuthRedirect<T>(Component: ComponentType<T>) {
 
-            return <Component {...this.props}/>
-        }
+    function RedirectComponent(props: MapStateToProps) {
+        const {isAuth, ...restProps} = props
+
+        if (!isAuth) return <Redirect to={'/login'}/>
+
+        return <Component {...restProps as T}/>
     }
 
     return connect(mapStateToProps)(RedirectComponent)
-};
+}
 
