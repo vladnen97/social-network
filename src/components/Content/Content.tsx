7e28c6a25d56ea28998c0a377ee516e8b2e14054
vladnen97 <darkdescent1991@gmail.com>
2023-04-29 23:53:12 +0300

    change import

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import c from './Content.module.css';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import {DialogsContainer} from './Dialogs/DialogsContainer';
+import DialogsContainer from './Dialogs/DialogsContainer';
 import {UsersContainer} from './Users/UsersContainer';
 import ProfileContainer from './Profile/ProfileContainer';
 import {Login} from './Login/Login';
