516fe93da84e4c699481cd10750c88ee090d8166
vladnen97 <darkdescent1991@gmail.com>
2023-05-04 00:59:02 +0300

    change placeholder

--- src/components/Content/Profile/SubmitPost/SubmitPost.tsx
+++ src/components/Content/Profile/SubmitPost/SubmitPost.tsx
@@ -24,7 +24,7 @@ export function SubmitPost({postTextValue, updateNewPostText, addPost}: PropsTyp
         <div className={s.submitPost}>
             <TextArea size={'large'}
                       autoSize={{ minRows: 2, maxRows: 8 }}
-                      placeholder={'Write a message...'}
+                      placeholder={"What's new?"}
                       value = {postTextValue}
                       onChange = {onChangeTextHandler}/>
             <Button size={'large'} onClick={addPostHandler} shape={'round'}> Post </Button>
