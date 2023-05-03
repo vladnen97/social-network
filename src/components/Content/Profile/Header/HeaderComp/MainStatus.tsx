48004e466bd1fc542294ddd5710c1e7b29baee89
vladnen97 <darkdescent1991@gmail.com>
2023-05-04 00:59:14 +0300

    refactoring

--- src/components/Content/Profile/Header/HeaderComp/MainStatus.tsx
+++ src/components/Content/Profile/Header/HeaderComp/MainStatus.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import s from '../Header.module.css';
+import {Input} from 'antd';
 
 type PropsType = {
     status: string | undefined
 }
 
-export class MainStatus extends React.Component<PropsType, {editMode: boolean}>{
+const {TextArea} = Input
+
+export class MainStatus extends React.Component<PropsType, { editMode: boolean }> {
     state = {
         editMode: false
     }
 
-    activateEditMode = () => {
-      this.setState({editMode: true})
+    activateEditMode = (): void => {
+        this.setState({editMode: true})
     }
-    deactivateEditMode = () => {
+    deactivateEditMode = (): void => {
         this.setState({editMode: false})
     }
 
     render() {
-        return <div>
+        return <div className={s.status}>
             {!this.state.editMode
-                ? <span className={s.status} onDoubleClick={this.activateEditMode}>{this.props.status ? this.props.status : 'status...'}</span>
-                : <input value={this.props.status} onBlur={this.deactivateEditMode} autoFocus/>
+                ? <span className={s.statusText}
+                        onDoubleClick={this.activateEditMode}>{this.props.status ? this.props.status : 'status...'}</span>
+                : <TextArea size={'middle'}
+                            autoSize={{minRows: 1, maxRows: 3}}
+                            placeholder={'Tell us something...'}
+                            onBlur={this.deactivateEditMode}
+                            maxLength={230}
+                            autoFocus
+                            value={this.props.status}/>
             }
         </div>
     }
