f9a42551881b92903efcae0d0960919ae41ca355
vladnen97 <darkdescent1991@gmail.com>
2023-05-03 23:59:42 +0300

    rewrite to class comp/ did state

--- src/components/Content/Profile/Header/HeaderComp/MainStatus.tsx
+++ src/components/Content/Profile/Header/HeaderComp/MainStatus.tsx
@@ -5,8 +5,17 @@ type PropsType = {
     status: string | undefined
 }
 
-export function MainStatus({status}: PropsType) {
-    return (
-        <div className={s.status}>{status ? status : 'status...'}</div>
-    );
+export class MainStatus extends React.Component<PropsType, {editMode: boolean}>{
+    state = {
+        editMode: false
+    }
+
+    render() {
+        return <div>
+            {!this.state.editMode
+                ? <span className={s.status}>{this.props.status ? this.props.status : 'status...'}</span>
+                : <input value={this.props.status}/>
+            }
+        </div>
+    }
 }
