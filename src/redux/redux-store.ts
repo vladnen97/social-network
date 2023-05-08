20ee919d6717fef28d45db563b96ba692458bc89
vladnen97 <darkdescent1991@gmail.com>
2023-05-09 02:51:15 +0300

    add form property from redux-form

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,6 +5,7 @@ import sideBarReducer from './sideBarReducer';
 import usersReducer, {UsersPageActionType} from './usersReducer';
 import {AuthActionsType, authReducer} from './authReducer';
 import thunk, {ThunkAction} from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
 
 
 const rootReducer = combineReducers({
@@ -12,11 +13,16 @@ const rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sideBar: sideBarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 export type RootStateType = ReturnType<typeof rootReducer>
-type AppActionTypes = UsersPageActionType | ProfilePageActionsType | DialogsPageActionsType | AuthActionsType
+type AppActionTypes =
+    UsersPageActionType
+    | ProfilePageActionsType
+    | DialogsPageActionsType
+    | AuthActionsType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AppActionTypes>
 
 const store: Store<RootStateType> = createStore(rootReducer, applyMiddleware(thunk));
@@ -25,4 +31,4 @@ const store: Store<RootStateType> = createStore(rootReducer, applyMiddleware(thu
 export default store;
 
 //@ts-ignore
-window.store= store
+window.store = store
