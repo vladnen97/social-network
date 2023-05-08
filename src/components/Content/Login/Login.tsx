095c737e0df8672c249bc6ed5898f61fa149ae51
vladnen97 <darkdescent1991@gmail.com>
2023-05-09 02:51:38 +0300

    login form comp with redux form

--- src/components/Content/Login/Login.tsx
+++ src/components/Content/Login/Login.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import s from './Login.module.css'
+import LoginForm from './LoginForm';
+
+export type FormDataType = {
+    email: string,
+    password: string,
+    rememberMe: boolean
+}
 
 export const Login = () => {
+
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
     return (
-        <div style={{textAlign: 'center', fontSize: '21px'}}>
-            Login
+        <div className={s.login}>
+            <h1>Login</h1>
+            <LoginForm onSubmit={onSubmit}/>
         </div>
     );
 };
---
+++ src/components/Content/Login/LoginForm.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import s from './Login.module.css';
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
+import {FormDataType} from './Login';
+
+const LoginForm = (props: InjectedFormProps<FormDataType>) => {
+    return (
+        <form className={s.form} onSubmit={props.handleSubmit}>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <Field name={'email'} component="input" placeholder={'enter your email'} required/>
+            </div>
+            <div>
+                <label htmlFor="password">Password: </label>
+                <Field name={'password'} component="input" placeholder={'enter your password'} required/>
+            </div>
+            <div>
+                <Field component="input" type="checkbox" name={'rememberMe'}/>
+                <label htmlFor={'rememberMe'}> Remember me</label>
+            </div>
+            <button>login</button>
+        </form>
+    )
+}
+
+export default reduxForm<FormDataType>({form: 'login'})(LoginForm)
