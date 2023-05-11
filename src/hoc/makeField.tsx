c8aacf721ffcc46f1a242b704f84fcd6fac51fe8
vladnen97 <darkdescent1991@gmail.com>
2023-05-11 06:09:05 +0300

    wrapp in form item

--- src/hoc/makeField.tsx
+++ src/hoc/makeField.tsx
@@ -1,7 +1,24 @@
+import { Form } from 'antd';
 import React from 'react';
 
-export const makeField = (Component: any) => ({ input, children, ...rest }: any) => {
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 }
+    },
+}
+
+export const makeField = (Component: any) => ({input, hasFeedback, label, children, meta: {touched, error, invalid}, ...rest}: any) => {
+    const hasError = touched && invalid;
     return (
-        <Component {...input} {...rest} children={children} />
+        <Form.Item
+            {...formItemLayout}
+            label={label}
+            validateStatus={hasError ? "error" : "success"}
+            hasFeedback={hasFeedback && hasError}
+            help={hasError && error}
+        >
+            <Component {...input} {...rest} children={children}/>
+        </Form.Item>
     )
 }
