67f918fdea7eda13ab6678f0fea1ac697159dc58
vladnen97 <darkdescent1991@gmail.com>
2023-05-13 22:45:34 +0300

    field compilable inputs

--- src/components/Content/Dialogs/addMessageForm.tsx
+++ src/components/Content/Dialogs/addMessageForm.tsx
@@ -3,15 +3,23 @@ import s from './Dialogs.module.css';
 import {Button, Input} from 'antd';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {makeField} from '../../../hoc/makeField';
+import {required} from '../../../utils/validators';
 
 const {TextArea} = Input
 const ATextarea = makeField(TextArea);
+const Textareareq = required(false)
 
 const AddMessageForm = (props: InjectedFormProps<{text: string}>) => {
     return (
         <form className={s.sendMessage} onSubmit={props.handleSubmit}>
-            <Field component={ATextarea} name={'text'} size={'large'} autoSize={{ minRows: 1, maxRows: 6 }}  placeholder={'Write a message...'}/>
-            <Button shape={'round'} size={'large'} type={'primary'} htmlType={'submit'}> send </Button>
+            <Field component={ATextarea}
+                   name={'text'}
+                   size={'large'}
+                   autoSize={{ minRows: 1, maxRows: 6 }}
+                   placeholder={'Write a message...'}
+                   hasFeedback
+                   validate={[Textareareq]}/>
+            <Button shape={'round'} size={'large'} type={'primary'} htmlType={'submit'} disabled={props.pristine}> send </Button>
         </form>
     )
 }
