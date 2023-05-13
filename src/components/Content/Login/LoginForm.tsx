77abcc9539c93f3e9673fd069c9fac6d34a5de40
vladnen97 <darkdescent1991@gmail.com>
2023-05-13 22:45:23 +0300

    field compilable inputs

--- src/components/Content/Login/LoginForm.tsx
+++ src/components/Content/Login/LoginForm.tsx
@@ -2,22 +2,63 @@ import React from 'react';
 import s from './Login.module.css';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {FormDataType} from './Login';
+import {makeField} from '../../../hoc/makeField';
+import {Input, Checkbox, Button, Form} from 'antd';
+import {maxLength, minLength, required} from '../../../utils/validators';
+
+const {Password} = Input
+const AInput = makeField(Input)
+const APassword = makeField(Password)
+const ACheckbox = makeField(Checkbox)
+
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0
+        },
+        sm: {
+            span: 14,
+            offset: 6
+        }
+    }
+}
+const maxLength12 = maxLength(12)
+const minLength5 = minLength(5)
+const InpurReq = required(true)
 
 const LoginForm = (props: InjectedFormProps<FormDataType>) => {
     return (
         <form className={s.form} onSubmit={props.handleSubmit}>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <Field name={'email'} component="input" placeholder={'enter your email'} required/>
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <Field name={'password'} component="input" placeholder={'enter your password'} required/>
-            </div>
-            <label>
-                <Field component="input" type="checkbox" name={'rememberMe'}/> Remember me
-            </label>
-            <button>login</button>
+
+
+            <Field name={'email'}
+                   label={'Email'}
+                   component={AInput}
+                   hasFeedback
+                   validate={[InpurReq]}
+                   size={'large'}
+                   type="email"
+                   placeholder={'enter your email'}/>
+
+
+            <Field name={'password'}
+                   label={'Password'}
+                   component={APassword}
+                   size={'large'}
+                   hasFeedback
+                   validate={[InpurReq, maxLength12, minLength5]}
+                   placeholder={'enter your password'}/>
+
+
+            <Field component={ACheckbox}
+                   label="Remember me"
+                   type="checkbox"
+                   name={'rememberMe'}/>
+
+            <Form.Item {...tailFormItemLayout}>
+                <Button size={'large'} htmlType={'submit'} shape={'round'} disabled={props.pristine}> Login </Button>
+            </Form.Item>
         </form>
     )
 }
