9767db10a417668a03737f642128c93343e39740
vladnen97 <darkdescent1991@gmail.com>
2023-05-13 22:45:30 +0300

    field compilable inputs

--- src/components/Content/Profile/SubmitPost/AddPostForm.tsx
+++ src/components/Content/Profile/SubmitPost/AddPostForm.tsx
@@ -3,17 +3,25 @@ import s from './SubmitPost.module.css'
 import {Button, Input} from 'antd';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {makeField} from '../../../../hoc/makeField';
+import {required} from '../../../../utils/validators';
 
 
 const {TextArea} = Input
 const ATextarea = makeField(TextArea);
+const TextAreareq = required(false)
 
 function AddPostForm(props: InjectedFormProps<{postText: string}>) {
 
     return (
         <form className={s.submitPost} onSubmit={props.handleSubmit}>
-            <Field component={ATextarea} name={'postText'} placeholder={"What's new?"} size={'large'} autoSize={{ minRows: 2, maxRows: 8 }}/>
-            <Button size={'large'} htmlType={'submit'} shape={'round'}> Post </Button>
+            <Field component={ATextarea}
+                   name={'postText'}
+                   placeholder={"What's new?"}
+                   size={'large'}
+                   autoSize={{ minRows: 2, maxRows: 8 }}
+                   hasFeedback
+                   validate={[TextAreareq]}/>
+            <Button size={'large'} htmlType={'submit'} shape={'round'} disabled={props.pristine}> Post </Button>
         </form>
     )
 }
