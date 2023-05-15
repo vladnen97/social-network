48b4c8d76f04b9f3a429d0340f12b3d3a5d1a9d0
vladnen97 <darkdescent1991@gmail.com>
2023-05-16 02:08:33 +0300

    if authorized -> redirect to profile

--- src/components/Content/Login/Login.tsx
+++ src/components/Content/Login/Login.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import s from './Login.module.css'
 import LoginForm from './LoginForm';
+import {connect} from 'react-redux';
+import {login} from '../../../redux/authReducer';
+import {RootStateType} from '../../../redux/redux-store';
+import {Redirect} from 'react-router-dom';
 
 export type FormDataType = {
     email: string,
     password: string,
     rememberMe: boolean
 }
+type MapStateToPropsType = {
+    isAuth: boolean
+}
 
-export const Login = () => {
+type LoginPropsType =MapStateToPropsType & {
+    login: (email: string, password: string, rememberMe: boolean) => void
+}
+
+const Login = (props: LoginPropsType) => {
 
     const onSubmit = (formData: FormDataType) => {
-        console.log(formData)
+        props.login(formData.email, formData.password, formData.rememberMe)
     }
 
+    if (props.isAuth) return <Redirect to={'/profile'}/>
     return (
         <div className={s.login}>
             <h1>Login</h1>
             <LoginForm onSubmit={onSubmit}/>
         </div>
-    );
-};
+    )
+}
+
+const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
+    return {
+        isAuth: state.auth.isAuth
+    }
+}
+
+export default connect(mapStateToProps, {login})(Login)
 
