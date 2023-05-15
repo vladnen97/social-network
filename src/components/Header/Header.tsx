6455fa84fad7508a14543d979dd76d66a4f90ba9
vladnen97 <darkdescent1991@gmail.com>
2023-05-16 02:07:42 +0300

    logout button and logout logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import s from './Header.module.css';
 import {SvgIcon} from '../SideBar/SvgIcon';
 import {NavLink} from 'react-router-dom';
+import {Button} from 'antd';
 
 type PropsType = {
     login: string | null
     isAuth: boolean
+    logout: () => void
 }
 
-export function Header({login, isAuth}: PropsType) {
+export function Header({login, isAuth, logout}: PropsType) {
     return (
         <header className={s.header}>
             <div className={s.logo}>
@@ -17,7 +19,10 @@ export function Header({login, isAuth}: PropsType) {
             </div>
             {
                 isAuth
-                    ? <NavLink to={'/profile'}><h2 className={s.name}>{login}</h2> </NavLink>
+                    ? <div style={{display: 'flex', marginRight: '20px'}}>
+                        <NavLink to={'/profile'}><h2 className={s.name}>{login}</h2></NavLink>
+                        <Button onClick={logout} shape={'round'}>Logout</Button>
+                    </div>
                     : <NavLink to={'/login'} className={s.login}>Login</NavLink>
             }
         </header>
