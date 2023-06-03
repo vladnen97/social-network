05c89bac4c1e37cb7cbdca24b12f1ea2e0916478
vladnen97 <darkdescent1991@gmail.com>
2023-06-03 19:10:08 +0300

    add new state reducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,7 +5,8 @@ import sideBarReducer from './sideBarReducer';
 import usersReducer, {UsersPageActionType} from './usersReducer';
 import {AuthActionsType, authReducer} from './authReducer';
 import thunk, {ThunkAction} from 'redux-thunk';
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
+import {AppActionType, appReducer} from './app-reducer';
 
 
 const rootReducer = combineReducers({
@@ -14,6 +15,7 @@ const rootReducer = combineReducers({
     usersPage: usersReducer,
     sideBar: sideBarReducer,
     auth: authReducer,
+    app: appReducer,
     form: formReducer
 })
 
@@ -23,6 +25,7 @@ type AppActionTypes =
     | ProfilePageActionsType
     | DialogsPageActionsType
     | AuthActionsType
+    | AppActionType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AppActionTypes>
 
 const store: Store<RootStateType> = createStore(rootReducer, applyMiddleware(thunk));
