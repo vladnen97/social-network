1315899aabde33bfd6e607d2b929c87b5862b357
vladnen97 <darkdescent1991@gmail.com>
2023-06-03 19:11:22 +0300

    add initialize app handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, {ComponentType} from 'react';
 import './App.css';
-import {Content} from "./components/Content/Content";
+import {Content} from './components/Content/Content';
 import {SideBarContainer} from './components/SideBar/SideBarContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+import {withRouter} from 'react-router-dom';
+import {initializeApp} from './redux/app-reducer';
+import {RootStateType} from './redux/redux-store';
+import {PreLoader} from './components/Content/Users/PreLoader';
 
+type MapStateToPropsType = {
+    initialized: boolean
+}
+type AppPropsType = MapStateToPropsType & {
+    initializeApp: () => void
+}
 
-function App() {
-    return (
-            <div className={"app-wrapper"}>
+class App extends React.Component<AppPropsType, {}> {
+    componentDidMount() {
+        this.props.initializeApp()
+    }
+
+    render() {
+        return !this.props.initialized
+            ? <PreLoader/>
+            : <div className={'app-wrapper'}>
                 <HeaderContainer/>
                 <SideBarContainer/>
                 <Content/>
             </div>
-    );
+
+    }
 }
 
-export default App;
+const mapStateToProps = (state: RootStateType): MapStateToPropsType => ({
+    initialized: state.app.initialized
+})
+
+
+export default compose<ComponentType>(withRouter, connect(mapStateToProps, {initializeApp}))(App)
