c5ece139df5993bb316638766ea51311e43c33e2
vladnen97 <darkdescent1991@gmail.com>
2023-06-22 00:49:16 +0300

    auth reducer tests

---
+++ src/tests/auth-reducer.test.ts
@@ -0,0 +1,34 @@
+import {authReducer, setAuthUserData} from '../redux/authReducer';
+
+
+type StartStateType = {
+    id: number | null,
+    login: string | null,
+    email: string | null,
+    isAuth: boolean
+}
+let startState: StartStateType
+
+beforeEach(() => {
+    startState = {
+        id: null,
+        login: null,
+        email: null,
+        isAuth: false
+    }
+})
+
+it('user data should not be setted', () => {
+    const endState = authReducer(startState, setAuthUserData())
+
+    expect(endState).toEqual(startState)
+})
+
+it('user data should be setted', () => {
+    const endState = authReducer(startState, setAuthUserData(1, 'test@.com', 'test97', true))
+
+    expect(endState.id).toBe(1)
+    expect(endState.isAuth).toBe(true)
+    expect(endState.login).toBe('test97')
+})
+
