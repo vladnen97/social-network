0b476a13f3e8a317a5cb0d0cb4ab2128d7731fe7
vladnen97 <darkdescent1991@gmail.com>
2023-07-01 20:04:53 +0300

    feat: add lazy loading to profile, dialogs, users.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import c from './Content.module.css';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import DialogsContainer from './Dialogs/DialogsContainer';
-import UsersContainer from './Users/UsersContainer';
-import ProfileContainer from './Profile/ProfileContainer';
 import Login from './Login/Login';
 import {Empty} from 'antd';
+import {withSuspense} from '../../hoc/withSuspense';
+
+const DialogsContainer = React.lazy(() => import('./Dialogs/DialogsContainer'))
+const ProfileContainer = React.lazy(() => import('./Profile/ProfileContainer'))
+const UsersContainer = React.lazy(() => import('./Users/UsersContainer'))
 
 
 export function Content() {
     return (
         <div className={c.content}>
             <Switch>
-                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)}/>
 
-                <Route path="/dialogs/:userId?" render={() => <DialogsContainer/>}/>
-                <Route path="/users" render={() => <UsersContainer/>}/>
+                <Route path="/dialogs/:userId?" render={withSuspense(DialogsContainer)}/>
+                <Route path="/users" render={withSuspense(UsersContainer)}/>
 
                 <Route path="/music" render={() => <div style={{textAlign: 'center', fontSize: '21px'}}>Music</div>}/>
                 <Route path="/login" render={() => <Login/>}/>
