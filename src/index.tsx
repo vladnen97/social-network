09624c696a11e040788dcfeff7696a4a25754758
vladnen97 <darkdescent1991@gmail.com>
2023-07-01 23:27:27 +0300

    feat: homepage, hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import './index.css';
 import 'antd/dist/antd.css';
@@ -9,11 +9,11 @@ import {Provider} from 'react-redux';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 )
 
