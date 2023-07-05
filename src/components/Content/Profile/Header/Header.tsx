a400116f885276c83e909e3dc4dac88b98b17a3a
vladnen97 <darkdescent1991@gmail.com>
2023-07-05 23:19:56 +0300

    feat: input type file, onChange callback

--- src/components/Content/Profile/Header/Header.tsx
+++ src/components/Content/Profile/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Header.module.css'
 import {MainAvatar} from './HeaderComp/MainAvatar';
 import {MainName} from './HeaderComp/MainName';
@@ -6,22 +6,26 @@ import {MainStatus} from './HeaderComp/MainStatus';
 import {HeaderPropsType} from './HeaderContainer';
 
 
-export function Header({ header, status, updateStatus }: HeaderPropsType) {
+export function Header({ header, status, updateStatus, uploadPhoto, match }: HeaderPropsType) {
+
+    const onPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
+        uploadPhoto(e.target.files?.[0])
+    }
 
     return (
         <div className={s.header}>
             <div className={s.cover}></div>
             <div className={s.inner}>
-                <MainAvatar online={false} url={header?.photos.large}/>
+                <MainAvatar online={false} url={header.photos?.large}/>
                 <div className={s.info}>
 
                     <MainName name={header?.fullName}/>
                     <MainStatus status={status} updateStatus={updateStatus}/>
 
                     <div className={s.fullInfo}>
-                        {/*<InfoTag iconId={iconId} title={title}/>*/}
                     </div>
                 </div>
+                {!match.params.userId && <input type="file" onChange={onPhotoSelected}/>}
             </div>
         </div>
     );
