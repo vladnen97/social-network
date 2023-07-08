3df4e1d9575016cde7a76bcacdda45571b4f1a5d
vladnen97 <darkdescent1991@gmail.com>
2023-07-08 21:20:57 +0300

    feat: handed over updateProfileData thunk

--- src/components/Content/Profile/Header/HeaderContainer.tsx
+++ src/components/Content/Profile/Header/HeaderContainer.tsx
@@ -1,10 +1,11 @@
 import {Header} from "./Header";
 import {connect} from 'react-redux';
 import {RootStateType} from '../../../../redux/redux-store';
-import {ProfilePageHeaderType, updateStatus, uploadPhoto} from '../../../../redux/profileReducer';
+import {ProfilePageHeaderType, updateProfileData, updateStatus, uploadPhoto} from '../../../../redux/profileReducer';
 import {compose} from 'redux';
 import {ComponentType} from 'react';
 import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {UpdateProfileData} from '../../../../api/api';
 
 type PathParamsType = {
     userId: string | undefined
@@ -16,6 +17,7 @@ type MapStateToPropsType = {
 export type HeaderPropsType = MapStateToPropsType & RouteComponentProps<PathParamsType> & {
     updateStatus: (status: string) => void
     uploadPhoto: (file: any) => void
+    updateProfileData: (model: UpdateProfileData) => void
 }
 
 const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
@@ -25,4 +27,4 @@ const mapStateToProps = (state: RootStateType): MapStateToPropsType => {
     }
 }
 
-export const HeaderContainer = compose<ComponentType>(connect(mapStateToProps, {updateStatus, uploadPhoto}), withRouter)(Header)
+export const HeaderContainer = compose<ComponentType>(connect(mapStateToProps, {updateStatus, uploadPhoto, updateProfileData}), withRouter)(Header)
